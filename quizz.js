b7ba4f1eda8c1b500a6e8cca9b98e222603e28af
Admin <beaucamps.dylan@gmail.com>
2016-12-31 15:09:26 +0100

    first commit

---
+++ quizz.js
@@ -0,0 +1,403 @@
+// *********************************************
+// ****************QUESTION1********************
+// *********************************************
+
+$("#reponseTrue1").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse1 , #reponseFalse2 , #reponseFalse3").css({
+		"background-color": "red",
+		"color": "black",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse1 , #reponseFalse2 , #reponseFalse3").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "black",
+		'transition': '0.9s',
+	});
+	$('#reponseTrue1').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+
+// *********************************************
+// ****************QUESTION2********************
+// *********************************************
+$("#reponseTrue2").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse4 , #reponseFalse5 , #reponseFalse6").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse4 , #reponseFalse5 , #reponseFalse6").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue2').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION3********************
+// *********************************************
+$("#reponseTrue3").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse7 , #reponseFalse8 , #reponseFalse9").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse7 , #reponseFalse8 , #reponseFalse9").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue3').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION4********************
+// *********************************************
+$("#reponseTrue4").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse10 , #reponseFalse11 , #reponseFalse12").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse10 , #reponseFalse11 , #reponseFalse12").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue4').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION5********************
+// *********************************************
+$("#reponseTrue5").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse13 , #reponseFalse14 , #reponseFalse15").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse13, #reponseFalse14 , #reponseFalse15").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue5').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION6********************
+// *********************************************
+$("#reponseTrue6").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse16 , #reponseFalse17 , #reponseFalse18").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse16, #reponseFalse17 , #reponseFalse18").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue6').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION7********************
+// *********************************************
+$("#reponseTrue7").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse19 , #reponseFalse20 , #reponseFalse21").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse19 , #reponseFalse20 , #reponseFalse21").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue7').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+
+// *********************************************
+// ****************QUESTION8********************
+// *********************************************
+
+$("#reponseTrue8").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse22 , #reponseFalse23 , #reponseFalse24").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse22 , #reponseFalse23 , #reponseFalse24").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue8').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+// *********************************************
+// ****************QUESTION9********************
+// *********************************************
+
+$("#reponseTrue9").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse25 , #reponseFalse26 , #reponseFalse27").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse25 , #reponseFalse26 , #reponseFalse27").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue9').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+
+// *********************************************
+// ****************QUESTION9********************
+// *********************************************
+
+$("#reponseTrue10").click(function(){
+	$(this).css({
+		"background-color": "green",
+		"color": "black",
+		'transition': '0.9s',
+		'font-size' : '25px',
+		'height': '35%'
+	});
+	$("#reponseFalse28 , #reponseFalse29 , #reponseFalse30").css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+$("#reponseFalse28 , #reponseFalse29 , #reponseFalse30").click(function(){
+	$(this).css({
+		"background-color": "red",
+		"color": "white",
+		'transition': '0.9s'
+	});
+	$('#reponseTrue10').css({
+		"background-color": "green",
+		"color": "white",
+		'transition': '0.9s'
+	});
+});
+
+
+
+
+
+$('#buttonNext1').click(function(){
+	$("#quest2").addClass('transform');
+	$('#quest1').addClass('none');
+});
+$('#buttonPrevious2').click(function() {
+	$("#quest2").removeClass('transform');
+	$('#quest1').removeClass('none');
+});
+
+
+$('#buttonNext2').click(function(){
+	$("#quest3").addClass('transform');
+	$('#quest2').removeClass('transform');
+});
+$('#buttonPrevious3').click(function() {
+	$("#quest2").removeClass('transform');
+	$('#quest3').removeClass('transform');
+	$('#quest2').addClass('transform');
+});
+
+
+$('#buttonNext3').click(function(){
+	$("#quest4").addClass('transform');
+	$('#quest3').removeClass('transform');
+});
+$('#buttonPrevious4').click(function() {
+	$("#quest3").removeClass('transform');
+	$('#quest4').removeClass('transform');
+	$('#quest3').addClass('transform');
+});
+
+
+$('#buttonNext4').click(function(){
+	$("#quest5").addClass('transform');
+	$('#quest4').removeClass('transform');
+});
+$('#buttonPrevious5').click(function() {
+	$("#quest4").removeClass('transform');
+	$('#quest5').removeClass('transform');
+	$('#quest4').addClass('transform');
+});
+
+
+$('#buttonNext5').click(function(){
+	$("#quest6").addClass('transform');
+	$('#quest5').removeClass('transform');
+});
+$('#buttonPrevious6').click(function() {
+	$("#quest5").removeClass('transform');
+	$('#quest6').removeClass('transform');
+	$('#quest5').addClass('transform');
+});
+
+
+
+$('#buttonNext6').click(function(){
+	$("#quest7").addClass('transform');
+	$('#quest6').removeClass('transform');
+});
+$('#buttonPrevious7').click(function() {
+	$("#quest6").removeClass('transform');
+	$('#quest7').removeClass('transform');
+	$('#quest6').addClass('transform');
+});
+
+
+
+$('#buttonNext7').click(function(){
+	$("#quest8").addClass('transform');
+	$('#quest7').removeClass('transform');
+});
+$('#buttonPrevious8').click(function() {
+	$("#quest7").removeClass('transform');
+	$('#quest8').removeClass('transform');
+	$('#quest7').addClass('transform');
+});
+
+
+
+$('#buttonNext8').click(function(){
+	$("#quest9").addClass('transform');
+	$('#quest8').removeClass('transform');
+});
+$('#buttonPrevious9').click(function() {
+	$("#quest8").removeClass('transform');
+	$('#quest9').removeClass('transform');
+	$('#quest8').addClass('transform');
+});
+
+
+
+
+$('#buttonNext9').click(function(){
+	$("#quest10").addClass('transform');
+	$('#quest9').removeClass('transform');
+});
+$('#buttonPrevious10').click(function() {
+	$("#quest9").removeClass('transform');
+	$('#quest10').removeClass('transform');
+	$('#quest9').addClass('transform');
+});
